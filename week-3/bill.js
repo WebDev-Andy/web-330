cc8839fce3676fdd594b3398c700d11409153ba0
Andy Britt <AndyB.WebDev@protonmail.com>
2022-06-06 11:17:35 -0400

    Resaurant App

---
+++ week-3/bill.js
@@ -0,0 +1,10 @@
+/*
+===========================================
+; File: .js
+; Author: Andrew Britt
+; Date: 6 Jun 2022
+; Description: 
+; Reference: 
+; Date reference accessed: 6 Jun 2022
+===========================================
+*/  
