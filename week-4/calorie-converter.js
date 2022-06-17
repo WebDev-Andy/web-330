93ba79a900638b814305291b7ee7f8fee37a3734
Andy Britt <AndyB.WebDev@protonmail.com>
2022-06-17 13:19:43 -0400

    Bare bones for calorie app

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,60 @@
+/*
+========================================
+; File: calorie-converter.js
+; Author: Andrew Britt
+; Date: 15 Jun 2022
+; Description:
+; Reference: WEB-330 assign pdf from Professors Krasso & Kumar
+; Date reference accessed: 15 June 2022
+========================================
+*/
+
+import { FoodModel } from "./food-model";
+
+export class CalorieConverter 
+{
+    constructor(ID, Name, Calories)
+    {
+        this.ID = ID;
+        this.Name = Name;
+        this.Calories = Calories;
+    }
+        static data = 
+        { 
+            ID: 1107,
+            Name: "Egg",
+            Calories: 78,
+            ID: 1008,
+            Name: "Apple",
+            Calories: 95,
+            ID: 1009,
+            Name: "Hamburger",
+            Calories: 354,
+            ID: 1009,
+            Name: Fries,
+            Calories: 400,
+            ID: 1009,
+            Name: "Banana",
+            Calories: 105,
+            ID: 1009,
+            Name: "Soda",
+            Calories: 150,
+        };
+        find() =>  
+}
+
+export class CalorieConverter
+{
+    constructor(id, name, calories)
+    {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+    static data() {
+
+    }
+}
+
+let egg = n
+
---
+++ week-4/food-model.js
@@ -0,0 +1,21 @@
+/*
+========================================
+; File: food-model.js
+; Author: Andrew Britt
+; Date: 15 Jun 2022
+; Description:
+; Reference: WEB-330 assign pdf from Professors Krasso & Kumar
+; Date reference accessed: 15 June 2022
+========================================
+*/  
+
+// Class constructor FoodModel
+export class FoodModel 
+{
+    constructor(id, name, calories)
+    {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+}
