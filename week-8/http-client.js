65be2f4c80ff271fd6077520c3cd10e649caed7b
Andy Britt <AndyB.WebDev@protonmail.com>
2022-07-11 15:13:15 -0400

    code for HttpClient class

---
+++ week-8/http-client.js
@@ -0,0 +1,32 @@
+/*
+; File: http-client.js
+; Author: Andrew Britt
+; Date: 11 July 2022
+; Description: JavaScript for Whatabooks2 assignment
+; Reference: Week-8 pdf web-330
+; Date accessed: 11 July 2022
+*/
+
+export class HttpClient 
+{
+    async get(url, params = "") 
+    {
+        // instantiates a new URL object
+        let urlObj = new URL(url);
+
+        // instantiates a new URLSearchParams object
+        let paramObj = new URLSearchParams(params);
+        
+        // assigning paramObj to url.search property
+        urlObj.search = paramObj;
+
+        // creates object literal
+        const res = await fetch(urlObj.toString(), 
+        { 
+            "GET", 
+        })
+
+        // return res object literal as JSON
+        return res.json();
+    }
+}
